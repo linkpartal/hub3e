2c35fa28d14297410c794126d4582cf74adb86b7
imane elkhettari <ielkhettari@fc-2i.com>
2016-03-01 17:01:14 +0000

    langue

--- web/peupler_DB.sql
+++ web/peupler_DB.sql
@@ -10,27 +10,27 @@ INSERT INTO `hobbies` (`id`, `hobbie`) VALUES
 
 
 INSERT INTO `Langue` (`id`, `langue`, `niveau`) VALUES
-(1,  'Francais', 'connaissance de base'),
+(1,  'Francais', 'connaissances de base'),
 (2,  'Francais', 'bonne connaissance'),
 (3,  'Francais', 'courant'),
 (4,  'Francais', 'langue maternelle'),
 
-(5,  'Anglais', 'connaissance de base'),
+(5,  'Anglais', 'connaissances de base'),
 (6,  'Anglais', 'bonne connaissance'),
 (7,  'Anglais', 'courant'),
 (8,  'Anglais', 'langue maternelle'),
 
-(9,  'Espagnol', 'connaissance de base'),
+(9,  'Espagnol', 'connaissances de base'),
 (10,  'Espagnol', 'bonne connaissance'),
 (11,  'Espagnol', 'courant'),
 (12,  'Espagnol', 'langue maternelle'),
 
-(13,  'Allemand', 'connaissance de base'),
+(13,  'Allemand', 'connaissances de base'),
 (14,  'Allemand', 'bonne connaissance'),
 (15,  'Allemand', 'courant'),
 (16,  'Allemand', 'langue maternelle'),
 
-(13,  'Arabe', 'connaissance de base'),
-(14,  'Arabe', 'bonne connaissance'),
-(15,  'Arabe', 'courant'),
-(16,  'Arabe', 'langue maternelle');
+(17,  'Arabe', 'connaissances de base'),
+(18,  'Arabe', 'bonne connaissance'),
+(19,  'Arabe', 'courant'),
+(20,  'Arabe', 'langue maternelle');
